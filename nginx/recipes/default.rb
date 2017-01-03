50645005729f17b8120e9cd7f1030ee523e964a3
pokhrelsubash28 <pokhrelsubash28@gmail.com>
2017-01-03 16:50:14 -0600

    this is secomd test

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+
+package 'epel-release' do
+action :install
+end
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/usr/share/nginx/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
